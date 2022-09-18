e4f9ba7f6235ed1de0cd76e18dde8aa1757fc34d
MrLOCAN <ldxthuy@gmail.com>
2022-09-18 21:04:10 +0700

    Comment default config CORSMiddleware

--- api/main.py
+++ api/main.py
@@ -19,8 +19,8 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=[],
-    max_age=300,
+    # expose_headers=[],
+    max_age=60,
 )
 
 @app.get("/")
